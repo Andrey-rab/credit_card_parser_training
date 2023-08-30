1a7db7a44b1d011a83a7cc59d4dc6d00f9c0c657
Andrey-rab <andreyraaab@gmail.com>
2023-08-30 16:36:45 +0300

    Создал новую папку для файлов csv

--- venv/parser.py
+++ venv/parser.py
@@ -13,7 +13,7 @@ HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'user-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
 }
-CSV = 'cards.csv'
+CSV = 'file_cards_csv/cards.csv'
 
 # Функция, которая получает html
 def get_html(url, params=''):
