8c49695587a203f0b95d8dee61216d7fedc03218
Andrey-rab <andreyraaab@gmail.com>
2023-08-30 14:45:24 +0300

    Функция возвращает список cards с содержимым 'title','link_product','brand','img_card'

--- venv/parser.py
+++ venv/parser.py
@@ -21,25 +21,31 @@ def get_html(url, params=''):
     #f = requests.
     return r
 # отладка
-html = get_html(URL)
-print(html)
+# html = get_html(URL)
+# print(html)
 
 #Получаем контент с html
 def get_content(html):
     # Получаем объект страницы
     soup = BeautifulSoup(html, 'html.parser')
     #парсим блоки карт
-    items = soup.select('tbody > tr')
+    items = soup.findAll('div', class_='xxx-listing-list__item')
     cards = []
-    # for item in items:
-    #     cards.append(
-    #         {
-    #             #'h4': item.find('h4').get_text(),
-    #             'title': item.find('div', class_='commercial-desc')
-    #         }
-    #     )
-    # return cards
-    print(items)
+    for item in items:
+        # асоциативный словарь. Здесь показываем, что будем собирать
+        cards.append(
+            {
+                #'h4': item.find('h4').get_text(),
+                'title': item.find('div', class_='xxx-listing-card__title-wrap').get_text(),
+                'link_product': item.find('div', class_='xxx-listing-card__title-wrap').find('a').get('href'),
+                'brand': item.find('div', class_='xxx-listing-card__title-wrap').find('span').get_text(),
+                #'img_card': item.find('div', class_='xxx-listing-card__img').find('img').get('src'),
+                'img_card': item.find('div', class_='xxx-listing-card__img').find('img').get('data-url-img')
+            }
+        )
+    return cards
+    #print(items)
 # отладка
 html = get_html(URL)
-get_content(html.text)
+content = get_content(html.text)
+print(content)
